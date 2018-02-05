b302b803a64bb9f99c1891f8d971302070896bd0
Jesper Dalby <jesperdalby@hotmail.com>
2018-02-05 11:33:17 +0100

    simpel

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simpelklasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Fornavn = "Jesper";
+            p1.Efternavn = "Dalby";
+            p1.Fødselsår = 1975;
+            Console.WriteLine(p1.FuldtNavn());
+            Console.WriteLine(p1.Alder());
+
+        }
+    }
+
+    public class Person
+    {
+        public string Fornavn;
+        public string Efternavn;
+        public int Fødselsår;
+
+        public string FuldtNavn()
+        {
+            return Fornavn + " " + Efternavn;
+        }
+        public int Alder()
+        {
+            int år = DateTime.Now.Year;
+            return år - this.Fødselsår;
+        }
+
+        public Person()
+        {
+            this.Fornavn = "";
+            this.Efternavn = "";
+        }
+
+        public Person(string fornavn, string efternavn)
+        {
+            this.Fornavn = fornavn.ToUpper();
+            this.Efternavn = efternavn.ToUpper();
+
+        }
+
+    }
+
+
+
+}
+
