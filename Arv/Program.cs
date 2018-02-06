3a08c636f505c08cb56eb4a5260212226fd65859
Jesper Dalby <jesperdalby@hotmail.com>
2018-02-06 11:16:51 +0100

    Arv

---
+++ Arv/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p = new Person();
+            p.Fornavn = "Jesper";
+
+            p.Efternavn = "Dalby";
+
+            Console.WriteLine(p.Fuldtnavn());
+
+            Elev e = new Elev();
+            e.Fornavn = "Jesper";
+
+            e.Efternavn = "Dalby";
+            e.Klasselokale = "33a";
+
+            Console.WriteLine(e.Fuldtnavn());
+
+            Instruktør i = new Instruktør();
+            i.Fornavn = "Jens";
+            i.Efternavn = "Hansen";
+            i.Nøgletal = 22;
+            Console.WriteLine(i.Fuldtnavn());
+
+        }
+    }
+
+    class Person
+    {
+
+        public string Fornavn { get; set; }
+
+        public string Efternavn { get; set; }
+
+
+        public string Fuldtnavn()
+        {
+            return this.Fornavn + " " + this.Efternavn;
+
+        }
+
+    }
+
+    public class Elev : Person
+    {
+        public string Klasselokale { get; set; }
+
+    }
+
+    public class Instruktør : Person
+    {
+        public int Nøgletal { get; set; }
+
+    }
+
+
+}
---
+++ Arvrandom/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvrandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom r = new UdvidetRandom();
+            Console.WriteLine(r.NextBool());
+        }
+    }
+
+    class UdvidetRandom : System.Random
+    {
+
+        public bool NextBool()
+        {
+            return this.Next(1, 1002) < 500;
+        }
+    }
+}
